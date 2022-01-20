48e0425ffa32926d49e070f4c6ce3f4b417e6578
Jake920626 <83159630+Jake920626@users.noreply.github.com>
2022-01-20 09:34:00 +0800

    整理新爆圖+一堆語法改動

--- bot.py
+++ bot.py
@@ -4,17 +4,23 @@ from discord.ext import commands
 import json
 import random
 import os
-from dotenv import load_dotenv
+import keep_alive
 
-token = os.getenv("token")
-
-with open ( '/Users/apple/Documents/GitHub/dis_bot/setting.json' , 'r' , encoding = 'utf8') as jfile:
-    jdata = json.load(jfile)
+jdata = {}
+token = os.environ['token']
+for filenames in os.listdir('dis_bot/text'):
+    with open ( 'setting.json' , 'r' , encoding = 'utf8') as jfile:
+        jdata.update(json.load(jfile))
 
 bot = commands.Bot(command_prefix='!10 ')
 
 @bot.event
 async def on_ready():
+
+    game = discord.Activity(type = discord.ActivityType.playing,name = '刀劍神域')
+    #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
+    await bot.change_presence(status=discord.Status.idle, activity=game)
+    
     print(">> bot is ready <<")
 
 @bot.command()
@@ -31,10 +37,5 @@ async def 星爆圖(ctx):
 async def help_me(ctx):
     await ctx.send(jdata['help'])
 
-@bot.command()
-async def reload(ctx):
-    
-    with open ( '/Users/apple/Documents/GitHub/dis_bot/setting.json' , 'r' , encoding = 'utf8') as jfile:
-        jdata = json.load(jfile)
-    await ctx.send("reloaded")
+keep_alive.keep_alive()
 bot.run(token)
