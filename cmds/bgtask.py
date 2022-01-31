8cf3da23503f18c8e7607622c02bb3be68f8c10c
Jake920626 <83159630+Jake920626@users.noreply.github.com>
2022-01-31 17:38:49 +0800

    更新：自動星爆
    解決時區問題，改變資料儲存，可以不用在reload後重新設定

--- cmds/bgtask.py
+++ cmds/bgtask.py
@@ -11,19 +11,31 @@ jdata = {}
 
 jdata = load('text')
 
+"""
+此類別為背景作業類別，用來進行在背景的回圈作業
+"""
 class bgtask (core.classinit.Cog_Extension):
 
     def __init__(self,bot):
         super().__init__(bot)
-        self.channels = []
+        print(jdata)
+        self.channels = jdata['channels']
         self.now = datetime.datetime.now().strftime('%H:%M:%S')
         self.used = False
+        self.loop_running = jdata['loop_running']
+
+        if self.loop_running == 1:
+            self.ever_day_48763.start()
 
     @commands.command()
     async def set_channel(self,ctx):
+        global jdata
         if str(ctx.author.id) in jdata['admin']:
             if ctx.channel.id not in self.channels: 
                 self.channels.append(ctx.channel.id) 
+                channels_updates = {'channels':self.channels,'loop_running':self.loop_running}
+                with open('text/channels.json','w')as updates:
+                    json.dump(channels_updates,updates)
                 await ctx.send('設定完畢')
             else:
                 await ctx.send('已設定過')
@@ -37,6 +49,10 @@ class bgtask (core.classinit.Cog_Extension):
                 await ctx.send('已在執行')
             else:
                 self.ever_day_48763.start()
+                self.loop_running = 1
+                channels_updates = {'channels':self.channels,'loop_running':self.loop_running}
+                with open('text/channels.json','w')as updates:
+                    json.dump(channels_updates,updates)
                 await ctx.send('迴圈開始')
         else:
             await ctx.send('你沒有權限！')
@@ -46,6 +62,10 @@ class bgtask (core.classinit.Cog_Extension):
         if str(ctx.author.id) in jdata['admin']:
             if self.ever_day_48763.is_running():
                 self.ever_day_48763.stop()
+                self.loop_running = 0
+                channels_updates = {'channels':self.channels,'loop_running':self.loop_running}
+                with open('text/channels.json','w')as updates:
+                    json.dump(channels_updates,updates)
                 await ctx.send('迴圈終止')
             else:
                 await ctx.send('尚未啟動')
